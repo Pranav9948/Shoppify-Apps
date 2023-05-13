66c6fa5983a61a50793eacff6e35a1ecfe97fca9
Pranav <pranavat9948@gmail.com>
2023-05-13 18:57:11 +0530

    shoppify app

---
+++ web/frontend/components/index.js
@@ -0,0 +1,6 @@
+
+
+export { QRCodeIndex } from "./QRCodeIndex";
+export { QRCodeForm } from "./QRCodeForms";
+export { ProductsCard } from "./ProductsCard";
+export * from "./providers";
---
+++ web/frontend/index.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import { QRCodeIndex } from "./components/QRCodeIndex";
+
+
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("app"));
